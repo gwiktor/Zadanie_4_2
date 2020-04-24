d60eaaafb8d839d457ab57acff9a0cc58df6f2b3
Wiktor Gabrys <gabrys.wiktor@gmail.com>
2020-04-24 20:12:41 +0200

    Define function 'palindrom' and check if everything works

--- zadanie_4_2.py
+++ zadanie_4_2.py
@@ -1,1 +1,11 @@
 #Zadanie 4_2
+def palindrom(slowo):
+  if slowo[::-1] == slowo:
+    print(slowo, slowo[::-1])
+    return True
+  else:
+    print("To nie palindrom")
+    return False
+    
+palindrom("oko")
+palindrom("słoń")
